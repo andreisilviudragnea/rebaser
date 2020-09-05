004ad02b18b6b47a73525fab507b9d04480e652e
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2020-09-05 13:09:55 +0300

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,18 @@ repositories {
     mavenCentral()
 }
 
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.8.1.202007141445-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:5.8.1.202007141445-r")
+    implementation("org.kohsuke:github-api:1.116")
 }
