5d8df6b646b12dd55ec993bd26efe308a53570f1
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-05-20 16:27:40 +0300

    wip3

--- src/main.rs
+++ src/main.rs
@@ -105,7 +105,7 @@ fn rebase(pr: &PullRequest, repo: &Repository) -> bool {
     println!("Current HEAD is {}", current_head_name);
 
     repo.set_head(&format!("refs/heads/{}", head_ref)).unwrap();
-    repo.checkout_head(None).unwrap();
+    repo.checkout_index(None, None).unwrap();
 
     let head = repo.head().unwrap();
     println!("Current HEAD is {}", head.name().unwrap());
@@ -125,11 +125,11 @@ fn rebase(pr: &PullRequest, repo: &Repository) -> bool {
     //
     // rebase.abort().unwrap();
     //
-    repo.set_head(current_head_name).unwrap();
-    repo.checkout_head(None).unwrap();
-
-    let head = repo.head().unwrap();
-    println!("Current HEAD is {}", head.name().unwrap());
+    // repo.set_head(current_head_name).unwrap();
+    // repo.checkout_head(None).unwrap();
+    //
+    // let head = repo.head().unwrap();
+    // println!("Current HEAD is {}", head.name().unwrap());
 
     false
 }
