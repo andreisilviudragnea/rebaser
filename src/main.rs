d40ccf63087d9169bbea98ca15fccf988c744d58
Andrei Silviu Dragnea <dragnea@adobe.com>
2021-05-23 18:44:11 +0300

    url bug

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use git2::Repository;
 
 use crate::git::{fetch, get_all_my_safe_prs, rebase_and_push};
+use url::Url;
 
 mod git;
 
 #[tokio::main]
 async fn main() {
+    let url = Url::parse("https://git.corp.adobe.com/api/v3").unwrap();
+    let result = Url::options()
+        .base_url(Some(&url))
+        .parse("repos/IMS/ims/pulls");
+
     let repo = Repository::discover(".").unwrap();
 
     let mut origin_remote = repo.find_remote("origin").unwrap();
